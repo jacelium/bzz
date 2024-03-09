a44e2e2669d6e0f4783ea4a78b23df4efb08fe25
Gareth Morgan <jacelium@gmail.com>
2024-03-09 18:45:53 +0000

    Refinements (#1)

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 # Config
-deny_list = None # List of names. If set, these users will not trigger a response
-allow_list = ['jacel'] # List of names. If set, these are the only users who will trigger a response
+deny_list = None # List of account names. If set, these users will not trigger a response
+allow_list = ['jacel@m.prettyshiny.org'] # List of account names. If set, these are the only users who will trigger a response
 scaler = 1 # All triggers will be scaled by this amount. e.g. to half all triggers, set this to 0.5 or 1/2
 poll_interval = 10 # The minimum space between shocks. Also the interval for polling for replies.
 verbose = False # Extra output
+closed_marker = '[FINISHED]' # Appended to a post's CW when closing it
+close_stats = True # Whether to generate stats on close
 
 # If True, only direct responses to the original post will be counted. If False, all children are considered
 strict = False
@@ -16,10 +18,12 @@ post_privacy = "unlisted" # direct, private unlisted public
 
 # File paths - files are used for storing some mostly ephemeral stuff and also config/logs
 # TODO: Should use actual Python logging but this is quick-and-dirty
-logfilepath = './log.txt'
-lastfile = './last.txt'
-credsfile = './creds.json'
-targetfile = './target.txt'
+logfilepath = './log.txt' # logfile
+lastfilepath = './last.txt' # last processed file
+credsfilepath = './creds.json' # credentials file
+targetfilepath = './target.txt' # target_id for current post
+
+knownfilepath = './known.txt' # stores previously seen names for stats
 
 #############################################################
 # Creds file should be a JSON file in this format:
@@ -44,8 +48,8 @@ from mastodon import Mastodon
 # TODO: Should use an actual config library but quick-and-dirty
 creds = {}
 
-with open(credsfile, 'r') as config_file:
-  creds = json.loads(config_file.read())
+with open(credsfilepath, 'r') as creds_file:
+  creds = json.loads(creds_file.read())
 
 clientpath = f'./{creds["mast_appname"]}_client.secret'
 userpath = f'./{creds["mast_appname"]}_user.secret'
@@ -68,29 +72,97 @@ if deny_list:
 if allow_list:
   print(f'Allow list is {allow_list}')
 
+def generate_stats(post_id):
+  known = []
+  updated = []
+
+  with open(knownfilepath, 'w+') as knownfile:
+    known = set(knownfile.readlines())
+    updated = known.copy()
+
+  logs = []
+  with open(logfilepath, 'r') as logfile:
+    logs = logfile.readlines()
+  logs = list(filter(lambda x: x.startswith(str(post_id)), logs))
+
+  count = len(logs)
+  if count == 0:
+    return '\n\nNo triggers received! :('
+
+  newusers = set()
+  max = 0
+  total = 0
+  biggest = set()
+
+  for log in logs:
+    fields = log.split(',')
+    intensity, name = int(fields[3]), fields[4]
+    if name not in known:
+      updated.add(name)
+    if intensity >= max:
+      max = intensity
+      biggest.add(name.strip())
+    total += intensity
+
+  def format_userlist(list, sep=', @', last_sep=' and @'):
+    output = sep.join(list)
+    if sep in output:
+      output = last_sep.join(output.rsplit(sep, 1))
+    return output
+
+  biggest = format_userlist(biggest)
+  new_names = updated - known
+  #new_names_string = format_userlist(new_names)
+  with open(knownfilepath, 'w') as knownfile:
+    knownfile.writelines(updated)
+
+  return f'\n\nMax was {max} (from @{biggest})\nAverage was {total/count} across {count} triggers\n{len(new_names)} new users'
+
+arg = sys.argv[1] if len(sys.argv) > 1 else ''
+
 # If we're triggering a newpost, delete any old targetfile
 
 target_id = None
 
-try:
-  if len(sys.argv) > 1 and sys.argv[1] == 'newpost':
-    try:
-      os.remove(targetfile)
-    except FileNotFoundError:
-      pass
-
-  with open(targetfile, 'a'):
+if arg == 'newpost':
+  try:
+    os.remove(targetfilepath)
+  except FileNotFoundError:
     pass
-  with open(targetfile, 'r') as file:
+
+with open(targetfilepath, 'a') as file:
+  pass
+with open(targetfilepath, 'r') as file:
+  try:
     target_id = int(file.readlines()[0])
     print(f'Using existing post id: {target_id}')
-except:
-  pass
+  except IndexError:
+    if arg != 'newpost': print(f'Couldn\'t read post ID from file')
+
+# If we're closing a post, do so and exit
+if arg == 'closepost':
+  if not target_id:
+    print('No current target set; can\'t close off post')
+    exit()
+
+  stats_marker = ' and generating stats' if close_stats else ''
+  print(f'Closing post {target_id} by appending {closed_marker} to CW{stats_marker}')
+
+  post = m.status(target_id)
+  existing_cw = post['spoiler_text']
+  cw_text = f'{existing_cw} {closed_marker}' if closed_marker not in existing_cw else existing_cw
+  stats_text = ''
+
+  if close_stats:
+    stats_text = generate_stats(target_id)
+
+  m.status_update(target_id, f'{post_body}{stats_text}', spoiler_text=cw_text)
+  exit()
 
 if target_id is None:
   # Clear the lastseen file and post a start post
   try:
-    os.remove(lastfile)
+    os.remove(lastfilepath)
   except FileNotFoundError:
     pass
 
@@ -114,18 +186,19 @@ if target_id is None:
     print(f'Using provided status ID: {input_id}')
     target_id = int(input_id)
 
-  with open(targetfile, 'a'):
+  with open(targetfilepath, 'a') as file:
     pass
-  with open(targetfile, 'w') as file:
+  with open(targetfilepath, 'w+') as file:
+    print(f'writing {target_id} to file')
     file.writelines([f'{target_id}'])
 
 # Ensure lastfile exists
-with open(lastfile, 'a'):
+with open(lastfilepath, 'a'):
   pass
 
 last_action_id = None
 try:
-  with open(lastfile, 'r') as file:
+  with open(lastfilepath, 'r') as file:
     last_action_id = int(file.readlines()[0])
 except:
   pass
@@ -174,7 +247,7 @@ def doit(parse, act, last_action_id):
 
       last_action_id = item['id']
 
-      with open(lastfile, 'w') as file:
+      with open(lastfilepath, 'w') as file:
         if verbose: print(f'Last seen is now {last_action_id}')
         file.writelines([f'{last_action_id}'])
 
@@ -196,13 +269,14 @@ def parse(item):
   if matches:
     count = len(matches.group(0))-1
     intensity = count * 10
-    if deny_list is not None and item['account']['username'].lower() in deny_list:
-       print(f'Skipping {intensity} from {item["account"]["username"]}, sent {item["created_at"].isoformat()} [denylist]')
-    elif allow_list is not None and item['account']['username'].lower() not in allow_list:
-       print(f'Skipping {intensity} from {item["account"]["username"]}, sent {item["created_at"].isoformat()} [allowlist]')
+    acct = item['account']['acct']
+    if deny_list is not None and acct.lower() in deny_list:
+       print(f'Skipping {intensity} from {acct}, sent {item["created_at"].isoformat()} [denylist]')
+    elif allow_list is not None and acct.lower() not in allow_list:
+       print(f'Skipping {intensity} from {acct}, sent {item["created_at"].isoformat()} [allowlist]')
     else:
-       print(f'Pushing {intensity} from {item["account"]["username"]}, sent {item["created_at"].isoformat()}')
-       return([intensity, item['account']['username'], item['created_at']])
+       print(f'Pushing {intensity} from {acct}, sent {item["created_at"].isoformat()}')
+       return([intensity, acct, item['created_at']])
   return False
 
 # The second is what to actually do with what Parse produces.
@@ -213,7 +287,7 @@ ps = PishockAPI(creds['shock_key'], creds['shock_username'], creds['shock_sharec
 def act(item):
     intensity, user, sent = item
 
-    with open(logfilepath, 'a') as logfile:
+    with open(logfilepath, 'a+') as logfile:
       logfile.write(f'{target_id},{sent.isoformat()},{datetime.datetime.now().isoformat()},{intensity},{user}\n')
 
     print(f'Sending {intensity} ({intensity/100}) on behalf of {user}')
