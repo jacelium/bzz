93236183396827c94f3e6394f354c41e6b41e68e
Gareth Morgan <jacelium@gmail.com>
2024-03-10 20:07:51 +0000

    Moving things out to a class, updating sample app (#2)

--- app.py
+++ app.py
@@ -1,42 +1,6 @@
-# Config
-deny_list = None # List of account names. If set, these users will not trigger a response
-allow_list = None # List of account names. If set, these are the only users who will trigger a response
-
-scaler = 1/3 # All triggers will be scaled by this amount. e.g. to half all triggers, set this to 0.5 or 1/2
-
-poll_interval = 10 # The minimum space between shocks. Also the interval for polling for replies.
-verbose = False # Extra output
-closed_marker = '[FINISHED]' # Appended to a post's CW when closing it
-close_stats = True # Whether to generate stats on close
-
-# If True, only direct responses to the original post will be counted. If False, all children are considered
-strict = False
-
-# These will be posted if target_id is unset
-
-post_body = """Fully automated and with some ability to close down and report on a session now!"
-
-"Oh no the Pishock is really easily scriptable and I might've written an integration that will shock me every time someone replies to this toot with 'bzz', with the number of Zs from 1-10 corresponding to the intensity from 10-100!"
-"""
-post_cw = "kink, painplay, remote toys"
-post_privacy = "public" # direct, private unlisted public
-
-# File paths - files are used for storing some mostly ephemeral stuff and also config/logs
-# TODO: Should use actual Python logging but this is quick-and-dirty
-logfilepath = './log.txt' # logfile
-lastfilepath = './last.txt' # last processed file
-credsfilepath = './creds.json' # credentials file
-targetfilepath = './target.txt' # target_id for current post
-
-knownfilepath = './known.txt' # stores previously seen names for stats
-
 #############################################################
-# Creds file should be a JSON file in this format:
+# Shocker creds file should be a JSON file in this format:
 #{
-#  "mast_username": "mastodon username",
-#  "mast_password": "mastodon password",
-#  "mast_appname": "The Mastodon app name",
-#  "mast_baseurl": "Your Mastodon instance's base URL",
 #  "shock_key": "The key from your share link (looks like a guid)",
 #  "shock_username": "Your pishock username",
 #  "shock_sharecode": "Your pishock sharecode",
@@ -44,49 +8,105 @@ knownfilepath = './known.txt' # stores previously seen names for stats
 #}
 #############################################################
 
-# Config done
-
-import json, os, sys, re, time, datetime
+import json, os, sys, re, time, datetime, random
 from pishockpy import PishockAPI
-from mastodon import Mastodon
+from bzz import Bzz
 
-# TODO: Should use an actual config library but quick-and-dirty
+# All triggers will be scaled by this amount. e.g. to half all triggers, set this to 0.5 or 1/2
+scaler = 1
+
+# Utility functions
+
+def format_userlist(input_list, sep=', ', last_sep=' and '):
+  """ Takes a list and outputs it in the format:
+      Item1, Item2, Item3 and Item4
+  """
+
+  output = sep.join(input_list)
+  if sep in output:
+    output = last_sep.join(output.rsplit(sep, 1))
+  return output
+
+##################################################################
+# We need to provide two methods. The first, Parse, takes
+# a post and extracts some information from it, then returns
+# an array of that information to be queued and carried out
+# by Act.
+# If we're skipping, return False instead.
+# This method is passed the status_dict of the current post
+# and the config object for the instance.
+##################################################################
+
+def parse(item, config):
+  """ Checks if a post matches a regex. If so, calculates
+      intensity based on the length of the match and pushes:
+      [intensity, source account, created datetime]
+      If disallowed by allow or deny list, returns False
+  """
+
+  post = item['content']
+
+  matches = re.search('[bB][z]{1,10}', post)
+
+  if matches:
+    count = len(matches.group(0))-1
+    intensity = count * 10
+    acct = item['account']['acct']
+    if config.deny_list is not None and acct.lower() in config.deny_list:
+       print(f'Skipping {intensity} from {acct}, sent {item["created_at"].isoformat()} [denylist]')
+    elif config.allow_list is not None and acct.lower() not in config.allow_list:
+       print(f'Skipping {intensity} from {acct}, sent {item["created_at"].isoformat()} [allowlist]')
+    else:
+       print(f'Pushing {intensity} from {acct}, sent {item["created_at"].isoformat()}')
+       return([intensity, acct, item['created_at']])
+  return False
+
+##################################################################
+# The second is what to actually do with what Parse produces.
+##################################################################
+
 creds = {}
 
-with open(credsfilepath, 'r') as creds_file:
+with open('shock_creds.json', 'r') as creds_file:
   creds = json.loads(creds_file.read())
 
-clientpath = f'./{creds["mast_appname"]}_client.secret'
-userpath = f'./{creds["mast_appname"]}_user.secret'
+# we need a Pishock instance for this handler
+ps = PishockAPI(creds['shock_key'], creds['shock_username'], creds['shock_sharecode'], creds['shock_appname'])
 
-if not os.path.isfile(clientpath):
-  Mastodon.create_app(creds['mast_appname'], api_base_url=creds["mast_baseurl"], to_file=clientpath)
+def act(item, target_id, config):
+  """ Acts on the output from Parse.
+      Sends a shock of the relevant intensity and logs some
+      information for stats generation purposes. 
+  """
+  intensity, user, sent = item
 
-m = Mastodon(client_id=clientpath)
+  with open(config.logfilepath, 'a+') as logfile:
+    logfile.write(f'{target_id},{sent.isoformat()},{datetime.datetime.now().isoformat()},{intensity},{user}\n')
 
-if not os.path.isfile(userpath):
-  m.log_in(creds["mast_username"], creds["mast_password"], to_file=userpath)
+  print(f'Sending {intensity} ({intensity/100}) on behalf of {user}')
 
-m = Mastodon(access_token=userpath)
+  if scaler != 1:
+    print(f'Scaled trigger: {(intensity/100) * scaler * 100}')
 
-print(f'\nStarting {creds["mast_appname"]}')
-print(f'Scaler is {scaler}')
+  ps.shock((intensity / 100) * scaler, 1)
 
-if deny_list:
-  print(f'Deny list is {deny_list}')
-if allow_list:
-  print(f'Allow list is {allow_list}')
+##################################################################
+# We can also optionally pass a stat generation method as a kwarg.
+##################################################################
 
-def generate_stats(post_id):
-  known = []
-  updated = []
+def generate_stats(post_id, config):
+  """ Reads logged items and generates some stats based on them.
+      Returns a string that will be appended to the closed post. 
+  """
 
-  with open(knownfilepath, 'w+') as knownfile:
-    known = set(knownfile.readlines())
+  known, updated = None, None
+
+  with open(config.knownfilepath, 'r+') as knownfile:
+    known = set(map(lambda x: x.strip(), knownfile.readlines()))
     updated = known.copy()
 
   logs = []
-  with open(logfilepath, 'r') as logfile:
+  with open(config.logfilepath, 'r') as logfile:
     logs = logfile.readlines()
   logs = list(filter(lambda x: x.startswith(str(post_id)), logs))
 
@@ -95,212 +115,62 @@ def generate_stats(post_id):
     return '\n\nNo triggers received! :('
 
   newusers = set()
-  max = 0
+  max_intensity = 0
   total = 0
   biggest = set()
 
   for log in logs:
     fields = log.split(',')
-    intensity, name = int(fields[3]), fields[4]
+    intensity, name = int(fields[3]), fields[4].strip()
     if name not in known:
       updated.add(name)
-    if intensity >= max:
-      max = intensity
-      biggest.add(name.strip())
+    if intensity >= max_intensity:
+      max_intensity = intensity
+      biggest.add(name)
     total += intensity
 
-  def format_userlist(list, sep=', @', last_sep=' and @'):
-    output = sep.join(list)
-    if sep in output:
-      output = last_sep.join(output.rsplit(sep, 1))
-    return output
+  biggest_count = 3
+  biggest_names  = list(random.sample(biggest, min(biggest_count, len(biggest))))
+  biggest_names = [f'@{x}' for x in biggest_names]
+  if len(biggest) > biggest_count:
+    biggest_names.append('others')
+  biggest_string = format_userlist(biggest_names)
 
-  biggest = format_userlist(biggest)
+  new_names_count = 3
   new_names = updated - known
-  #new_names_string = format_userlist(new_names)
-  with open(knownfilepath, 'w') as knownfile:
-    knownfile.writelines(updated)
+  sampled_names = list(random.sample(new_names, min(new_names_count, len(new_names))))
+  sampled_names = [f'@{x}' for x in sampled_names]
+  if len(new_names) > new_names_count:
+    sampled_names.append('the rest')
+  new_names_string = format_userlist(sampled_names)
+  with open(config.knownfilepath, 'w') as knownfile:
+    knownfile.writelines([f'{x}\n' for x in updated])
 
-  return f'\n\nMax was {max} (from @{biggest})\nAverage was {total/count} across {count} triggers\n{len(new_names)} new users'
+  return f"""
+
+Closed at {datetime.datetime.strftime(datetime.datetime.now(), '%H:%M, %b %d')}
+
+- Max intensity was {max_intensity} (from {biggest_string})
+- Average intensity was ~{int(total/count)} across {count} triggers
+- {len(new_names) if len(new_names) > 0 else "No"} new visitors this time!{f" (Hi {new_names_string}!)" if len(new_names_string) > 0 else " :("}
+"""
+
+##################################################################
+# Start monitoring. Pass our two methods to the harness, along with our saved last_action_id if we have one.
+##################################################################
+
+print(f'Scaler is {scaler}')
+
+bzz = Bzz(parse, act, config_file='config.json', stats_function=generate_stats)
 
 arg = sys.argv[1] if len(sys.argv) > 1 else ''
 
-# If we're triggering a newpost, delete any old targetfile
-
-target_id = None
-
-if arg == 'newpost':
-  try:
-    os.remove(targetfilepath)
-  except FileNotFoundError:
-    pass
-
-with open(targetfilepath, 'a') as file:
-  pass
-with open(targetfilepath, 'r') as file:
-  try:
-    target_id = int(file.readlines()[0])
-    print(f'Using existing post id: {target_id}')
-  except IndexError:
-    if arg != 'newpost': print(f'Couldn\'t read post ID from file')
-
-# If we're closing a post, do so and exit
 if arg == 'closepost':
-  if not target_id:
-    print('No current target set; can\'t close off post')
-    exit()
-
-  stats_marker = ' and generating stats' if close_stats else ''
-  print(f'Closing post {target_id} by appending {closed_marker} to CW{stats_marker}')
-
-  post = m.status(target_id)
-  existing_cw = post['spoiler_text']
-  cw_text = f'{existing_cw} {closed_marker}' if closed_marker not in existing_cw else existing_cw
-  stats_text = ''
-
-  if close_stats:
-    stats_text = generate_stats(target_id)
-
-  m.status_update(target_id, f'{post_body}{stats_text}', spoiler_text=cw_text)
+  bzz.close_post()
   exit()
+elif arg == 'newpost':
+  bzz.create_post()
+else:
+  bzz.attach_to_post()
 
-if target_id is None:
-  # Clear the lastseen file and post a start post
-  try:
-    os.remove(lastfilepath)
-  except FileNotFoundError:
-    pass
-
-  line_length = 70
-  body_len = len(post_body)
-  body = [ post_body[i:i+line_length] for i in range(0, body_len, line_length) ]
-  separator = '\n'
-
-  print(f'\nWill post new {post_privacy.upper()} status in { "STRICT" if strict else "NON-STRICT"} mode:\n')
-  print('-----------------------------------------------------------')
-  print(f' CW: {post_cw}\n\n {separator.join(body)}')
-  print('\n-----------------------------------------------------------\n')
-
-  input_id = input('Please enter an existing status ID to listen to, or just hit enter\nto post and listen to the above status. ctrl-C cancels.\n> ')
-
-  if input_id == '':
-    new_post = m.status_post(post_body, visibility=post_privacy, spoiler_text=post_cw)
-    target_id = new_post['id']
-    print(f'Post made. View it at {new_post["url"]}')
-  else:
-    print(f'Using provided status ID: {input_id}')
-    target_id = int(input_id)
-
-  with open(targetfilepath, 'a') as file:
-    pass
-  with open(targetfilepath, 'w+') as file:
-    file.writelines([f'{target_id}'])
-
-# Ensure lastfile exists
-with open(lastfilepath, 'a'):
-  pass
-
-last_action_id = None
-try:
-  with open(lastfilepath, 'r') as file:
-    last_action_id = int(file.readlines()[0])
-except:
-  pass
-
-def find_index(predicate, l):
-  for i,e in enumerate(l):
-    if predicate(e): return i
-  return None
-
-def doit(parse, act, last_action_id):
-  queue = []
-
-  while True:
-    if verbose: print(f'Fetching notifications. Last seen is {last_action_id}')
-    # TODO: This is kind of brute-forcey. Can we filter by last seen time?
-    notifications = m.status_context(target_id)['descendants']
-
-    if strict:
-      target_replies = list(filter(lambda x: x['in_reply_to_id'] == target_id, notifications))
-    else:
-      target_replies = notifications
-
-    if target_replies == []:
-      time.sleep(poll_interval)
-      continue
-
-    target_replies.sort(key=lambda x: x['created_at'])
-
-    subset = []
-
-    if last_action_id is None:
-      subset = target_replies
-    else:
-      last_actioned = find_index(lambda x: x['id'] == last_action_id, target_replies)
-      subset = target_replies[last_actioned+1:]
-
-    subset = list(subset)
-
-    if len(subset) == 0:
-      if verbose: print('Nothing to process.')
-
-    for item in list(subset):
-      result = parse(item)
-      if result:
-        queue.append(result)
-
-      last_action_id = item['id']
-
-      with open(lastfilepath, 'w') as file:
-        if verbose: print(f'Last seen is now {last_action_id}')
-        file.writelines([f'{last_action_id}'])
-
-    try:
-      act(queue.pop(0))
-    except:
-      if verbose: print('Nothing to send')
-
-    time.sleep(poll_interval)
-
-# We need to provide two methods. The first, Parse, takes a post and extracts some information from it.
-# If we're skipping, return False instead.
-
-def parse(item):
-  post = item['content']
-
-  matches = re.search('b[z]{1,10}', post)
-
-  if matches:
-    count = len(matches.group(0))-1
-    intensity = count * 10
-    acct = item['account']['acct']
-    if deny_list is not None and acct.lower() in deny_list:
-       print(f'Skipping {intensity} from {acct}, sent {item["created_at"].isoformat()} [denylist]')
-    elif allow_list is not None and acct.lower() not in allow_list:
-       print(f'Skipping {intensity} from {acct}, sent {item["created_at"].isoformat()} [allowlist]')
-    else:
-       print(f'Pushing {intensity} from {acct}, sent {item["created_at"].isoformat()}')
-       return([intensity, acct, item['created_at']])
-  return False
-
-# The second is what to actually do with what Parse produces.
-
-# we need a Pishock instance for this handler
-ps = PishockAPI(creds['shock_key'], creds['shock_username'], creds['shock_sharecode'], creds['shock_appname'])
-
-def act(item):
-    intensity, user, sent = item
-
-    with open(logfilepath, 'a+') as logfile:
-      logfile.write(f'{target_id},{sent.isoformat()},{datetime.datetime.now().isoformat()},{intensity},{user}\n')
-
-    print(f'Sending {intensity} ({intensity/100}) on behalf of {user}')
-
-    if scaler != 1:
-      print(f'Scaled shock: {(intensity/100) * scaler * 100}')
-
-    ps.shock((intensity / 100) * scaler, 1)
-
-# Start monitoring. Pass our two methods to the harness, along with our saved last_action_id if we have one.
-
-doit(parse, act, last_action_id)
+bzz.run()
---
+++ bzz/bzz.py
@@ -0,0 +1,209 @@
+import logging
+import threading
+import json, os, time
+from mastodon import Mastodon
+from .config import Config
+from .util import find_index
+
+#############################################################
+# Mastodon Creds file should be a JSON file in this format:
+#{
+#  "mast_username": "mastodon username",
+#  "mast_password": "mastodon password",
+#  "mast_appname": "The Mastodon app name",
+#  "mast_baseurl": "Your Mastodon instance's base URL"
+#}
+#############################################################
+
+class Bzz:
+  def __init__(self, parse_function, act_function, **kwargs):
+
+    # Bootstrap our config file
+    self.config_file_path = kwargs.get('config_file', './bzz.conf')
+    try:
+      self.c = Config.from_file(self.config_file_path)
+    except ValueError as e:
+      print(f'Malformed configuration: {e}')
+      raise
+    except FileNotFoundError as e:
+      print(f'Config file does not exist. Creating a defaulted config under {self.config_file_path}.')
+      self.c = Config()
+      self.c.save(self.config_file_path)
+
+    self.parse_function = parse_function
+    self.act_function = act_function
+    self.stats_function = kwargs.get('stats_function', None)
+    if self.c.close_stats and self.stats_function is None:
+      print('*** Warning: `close_stats` is true and no stats function is set ***')
+
+    self.log = logging.getLogger(__name__)
+
+    self.queue = []
+    self.read_thread = None
+    self.target_id = None
+
+    # Setup Mastodon client
+    # TODO: Should use an actual config library but quick-and-dirty
+    creds = {}
+
+    with open(self.c.credsfilepath, 'r') as creds_file:
+      creds = json.loads(creds_file.read())
+
+    clientpath = f'./{creds["mast_appname"]}_client.secret'
+    userpath = f'./{creds["mast_appname"]}_user.secret'
+
+    if not os.path.isfile(userpath):
+      if not os.path.isfile(clientpath):
+        Mastodon.create_app(creds['mast_appname'], api_base_url=creds["mast_baseurl"], to_file=clientpath)
+
+      m = Mastodon(client_id=clientpath)
+      m.log_in(creds["mast_username"], creds["mast_password"], to_file=userpath)
+
+    self.app_name = creds["mast_appname"]
+    self.m = Mastodon(access_token=userpath)
+
+  def create_post(self):
+    try:
+      os.remove(self.c.targetfilepath)
+    except FileNotFoundError:
+      pass
+    self.attach_to_post(existing=False)
+
+  def attach_to_post(self, existing=True):
+    with open(self.c.targetfilepath, 'a') as file:
+      pass
+    with open(self.c.targetfilepath, 'r') as file:
+      try:
+        self.target_id = int(file.readlines()[0])
+        print(f'Using existing post id: {self.target_id}')
+      except IndexError:
+        if existing: print(f'Couldn\'t read post ID from file')
+
+    if self.target_id is None:
+      # Clear the lastseen file and post a start post
+      try:
+        os.remove(self.c.lastfilepath)
+      except FileNotFoundError:
+        pass
+
+      line_length = 70
+      body_len = len(self.c.post_body)
+      body = [ self.c.post_body[i:i+line_length] for i in range(0, body_len, line_length) ]
+      separator = '\n'
+
+      print(f'\nWill post new {self.c.post_privacy.upper()} status in { "STRICT" if self.c.strict else "NON-STRICT"} mode:\n')
+      print('-----------------------------------------------------------')
+      print(f' CW: {self.c.post_cw}\n\n {separator.join(body)}')
+      print('\n-----------------------------------------------------------\n')
+
+      input_id = input('Please enter an existing status ID to listen to, or just hit enter\nto post and listen to the above status. ctrl-C cancels.\n> ')
+
+      if input_id == '':
+        new_post = self.m.status_post(self.c.post_body, visibility=self.c.post_privacy, spoiler_text=self.c.post_cw)
+        self.target_id = new_post['id']
+        print(f'Post made. View it at {new_post["url"]}')
+      else:
+        print(f'Using provided status ID: {input_id}')
+        self.target_id = int(input_id)
+
+      with open(self.c.targetfilepath, 'a') as file:
+        pass
+      with open(self.c.targetfilepath, 'w+') as file:
+        file.writelines([f'{self.target_id}'])
+
+  def close_post(self):
+    self.attach_to_post()
+    # If we're closing a post, do so and exit
+    if not self.target_id:
+      print('No current target set; can\'t close off post')
+      exit()
+
+    stats_marker = ' and generating stats' if self.c.close_stats and self.stats_function is not None else ''
+    print(f'Closing post {self.target_id} by appending {self.c.closed_marker} to CW{stats_marker}')
+
+    post = self.m.status(self.target_id)
+    existing_cw = post['spoiler_text']
+    cw_text = f'{existing_cw} {self.c.closed_marker}' if self.c.closed_marker not in existing_cw else existing_cw
+
+    stats_text = ''
+    if self.c.close_stats and self.stats_function is not None:
+      stats_text = self.stats_function(self.target_id, self.c)
+
+    self.m.status_update(self.target_id, f'{self.c.post_body}{stats_text}', spoiler_text=cw_text)
+    exit()
+
+  def run(self):
+    print(f'\nStarting {self.app_name}')
+
+    if self.c.deny_list:
+      print(f'Deny list is {self.c.deny_list}')
+    if self.c.allow_list:
+      print(f'Allow list is {self.c.allow_list}')
+
+    self.queue = []
+
+    # Ensure lastfile exists
+    with open(self.c.lastfilepath, 'a'):
+      pass
+
+    self.last_action_id = None
+    try:
+      with open(self.c.lastfilepath, 'r') as file:
+        self.last_action_id = int(file.readlines()[0])
+    except:
+      pass
+
+    def read_method(queue):
+      while True:
+        if self.c.verbose: print(f'Fetching responses. Last seen is {self.last_action_id}')
+        # TODO: This is kind of brute-forcey. Can we filter by last seen time?
+        responses = self.m.status_context(self.target_id)['descendants']
+
+        if self.c.strict:
+          target_replies = list(filter(lambda x: x['in_reply_to_id'] == self.target_id, responses))
+        else:
+          target_replies = responses
+
+        if target_replies == []:
+          time.sleep(self.c.parse_interval)
+          continue
+
+        target_replies.sort(key=lambda x: x['created_at'])
+
+        subset = []
+
+        if self.last_action_id is None:
+          subset = target_replies
+        else:
+          last_actioned = find_index(lambda x: x['id'] == self.last_action_id, target_replies)
+          subset = target_replies[last_actioned+1:]
+
+        subset = list(subset)
+
+        if len(subset) == 0:
+          if self.c.verbose: print('Nothing to process.')
+
+
+        for item in list(subset):
+          result = self.parse_function(item, self.c)
+          if result:
+            self.queue.append(result)
+
+          self.last_action_id = item['id']
+
+          with open(self.c.lastfilepath, 'w') as file:
+            if self.c.verbose: print(f'Last seen is now {self.last_action_id}')
+            file.writelines([f'{self.last_action_id}'])
+        time.sleep(self.c.parse_interval)
+
+    print('Starting read thread...')
+    self.read_thread = threading.Thread(target=read_method, args=(self.queue,))
+    self.read_thread.start()
+
+    while True:
+      try:
+        self.act_function(self.queue.pop(0), self.target_id, self.c)
+      except Exception as e:
+        if self.c.verbose: print('Nothing to send')
+
+      time.sleep(self.c.act_interval)
